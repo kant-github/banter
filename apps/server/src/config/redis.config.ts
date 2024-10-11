b0d20a73ffbd0430800fa338c1b0c222b09d66e7
Rishi Kant <kantrishi7779@gmail.com>
2024-10-12 01:33:45 +0530

    trying to deploy

--- apps/server/src/config/redis.config.ts
+++ apps/server/src/config/redis.config.ts
@@ -4,13 +4,14 @@ import { Redis } from 'ioredis';
 dotenv.config();
 
 const url = process.env.REDIS_URL;
+console.log("url is ", url);
 
 
 if (!url) {
     throw new Error("REDIS_URL environment variable is not defined.");
 }
 
-const redis = new Redis("redis://localhost:6379");
+const redis = new Redis(url);
 
 
 export default redis;
