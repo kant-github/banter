0318f229741da9ae997388dc5595c36baebc2a7d
Rishi Kant <kantrishi7779@gmail.com>
2024-10-25 02:42:08 +0530

    made a minor UI change

--- apps/web/src/components/ui/CardHoverChatCards.tsx
+++ apps/web/src/components/ui/CardHoverChatCards.tsx
@@ -58,7 +58,7 @@ export default function CardList({
                         <AnimatePresence>
                             {hoveredIndex === idx && (
                                 <motion.span
-                                    className="absolute inset-0 h-full w-full bg-[#c9d0d4] dark:bg-gray-400 block rounded-3xl"
+                                    className="absolute inset-0 h-full w-full bg-[#c9d0d4] dark:bg-zinc-700 block rounded-3xl"
                                     layoutId="hoverBackground"
                                     initial={{ opacity: 0 }}
                                     animate={{
