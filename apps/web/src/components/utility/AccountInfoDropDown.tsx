027b0feb1e8465fb9a95bd75cb2bb1fc1bb52d9e
kant-github <kantrishi7779@gmail.com>
2024-11-15 02:43:44 +0530

    Improved the account information page

--- apps/web/src/components/utility/AccountInfoDropDown.tsx
+++ apps/web/src/components/utility/AccountInfoDropDown.tsx
@@ -2,9 +2,10 @@ import Image from "next/image";
 import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
 import CrossButton from "./CrossButton";
 import { fetchUser } from "fetch/fetchUser";
+import { FaEdit } from "react-icons/fa";
 import { CustomSession } from "app/api/auth/[...nextauth]/options";
 
-import { format } from 'date-fns';import { UserType } from "types";
+import { format } from 'date-fns'; import { UserType } from "types";
 
 interface Props {
     session: CustomSession;
@@ -30,7 +31,7 @@ export default function AccountInfoModal({ session, setAccountInfoDropDown }: Pr
 
     return (
         <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex items-center justify-center">
-            <div className="bg-white dark:bg-[#262629] dark:text-gray-200 p-6 rounded-lg shadow-lg w-8/12 relative">
+            <div className="bg-white dark:bg-zinc-800 dark:text-gray-200 p-6 rounded-lg shadow-lg w-6/12 relative">
                 <div className="flex justify-between">
                     <p className="text-lg font-semibold">
                         Account Information
@@ -41,37 +42,46 @@ export default function AccountInfoModal({ session, setAccountInfoDropDown }: Pr
                     See all your account details below
                 </p>
 
-                <div className="mt-6 flex w-full">
-                    <Image
-                        className="rounded-full mr-4"
-                        src={userData?.image!}
-                        width={80}
-                        height={80}
-                        alt="User Profile Image"
-                    />
-                    <div className="mb-2 w-full">
-                        <label className="block text-sm font-medium">Name</label>
-                        <div className="w-full border-[1px] border-zinc-700 px-4 py-2 rounded-[4px] font-thin text-sm">
-                            {userData?.name || session.user?.name || "No Name Available"}
+                <div className="bg-zinc-700 rounded-[4px] p-2 pl-8 mt-4">
+                    <div className="mt-6 flex w-full">
+                        <Image
+                            className="rounded-full mr-4"
+                            src={userData?.image!}
+                            width={80}
+                            height={80}
+                            alt="User Profile Image"
+                        />
+                        <div className="w-4/5 bg-zinc-600 font-thin text-[12px] px-4 py-2 relative">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi blanditiis error exercitationem reiciendis facere provident ab minus dolor cumque vero!
+                            <FaEdit className="absolute bottom-2 right-2 cursor-pointer" size={16} />
+                        </div>
+
+                    </div>
+
+                    <div className="flex flex-col gap-y-2">
+                        <div className="my-2">
+                            <div className="text-lg font-medium">Name</div>
+                            <div className="font-thin text-md">
+                                {userData?.name || session.user?.name || "No Name Available"}
+                            </div>
+                        </div>
+                        <div className="my-2">
+                            <div className="text-lg font-medium">Email</div>
+                            <div className="font-thin text-md">
+                                {userData?.email || session.user?.email || "No Email Available"}
+                            </div>
+                        </div>
+                        <div className="mb-2">
+                            <div className="text-lg font-medium">Joined At</div>
+                            <div className="font-thin text-md">
+                                {userData?.created_at
+                                    ? `Joined at: ${format(new Date(userData.created_at), 'MMMM dd, yyyy')}`
+                                    : "No Date Available"}
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className="flex-1">
-                    <div className="mb-2 gap-x-8">
-                        <label className="block text-sm font-medium w-20">Email:</label>
-                        <div className="w-full border-[1px] border-zinc-700 px-4 py-2 rounded-[4px] font-thin text-sm">
-                            {userData?.email || session.user?.email || "No Email Available"}
-                        </div>
-                    </div>
-                    <div className="mb-2 gap-x-8">
-                        <label className="block text-sm font-medium w-20">Created At:</label>
-                        <div className="w-full border-[1px] border-zinc-700 px-4 py-2 rounded-[4px] font-thin text-sm">
-                            {userData?.created_at
-                                ? `Joined at: ${format(new Date(userData.created_at), 'MMMM dd, yyyy')}`
-                                : "No Date Available"}
-                        </div>
-                    </div>
-                </div>
+
             </div>
         </div>
     );
